28547e03a18bda9fde7a4b31e9c5dc3c44bf8009
smakeneni <makeneni@gmail.com>
2018-03-18 22:44:49 -0400

    Sixth commit

--- plot4.R
+++ plot4.R
@@ -9,15 +9,15 @@ with(twodays,{
   plot(Global_active_power~Datetime,type="l",
        ylab="Global Active Power(kilowatts)",xlab="")
   plot(Voltage~Datetime,type="l",
-       ylab="Voltage (volt)", xlab="")
+       ylab="Voltage", xlab="datetime")
   plot(Sub_metering_1~Datetime,type="l",
-       ylab="Global Active Power(kilowatts)",xlab="")
+       ylab="Energy sub metering",xlab="")
   lines(Sub_metering_2~Datetime,col="Red")
   lines(Sub_metering_3~Datetime,col="Blue")
   legend("topright",col=c("black","red","blue"),lty=1,lwd=2,
-         legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+         c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="o")
   plot(Global_reactive_power~Datetime, type="l",
-       ylab="Global Reactive Power (kilowatts)", xlab="")
+       ylab="Global_reactive_power", xlab="datetime")
 })
 
 dev.copy(png,file="/Users/spandana/Desktop/plot4.png",height=480, width=480)
